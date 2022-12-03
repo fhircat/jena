a85306315db4792275676695cec61bdced20465f
Eric Prud'hommeaux <eric+github@w3.org>
2022-12-03 20:29:23 +0100

    migrating to plan B

--- jena-shex/src/main/java/org/apache/jena/shex/manifest/ValidationEntry.java
+++ jena-shex/src/main/java/org/apache/jena/shex/manifest/ValidationEntry.java
@@ -1,9 +1,8 @@
 package org.apache.jena.shex.manifest;
-public class ValidationEntry {
-    public String schemaLabel;
-    public String schema;
-    public String dataLabel;
-    public String data;
-    public String queryMap;
-    public String status;
+
+public class ValidationEntry extends ManifestEntry {
+
+    public static ValidationEntry newEntry() {
+        return null;
+    }
 }
