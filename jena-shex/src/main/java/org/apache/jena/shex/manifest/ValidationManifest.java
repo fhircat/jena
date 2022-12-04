dfa68c19c09b755e304651f5ee3b7739dd396dcd
cnanjo@gmail.com <cnanjo@gmail.com>
2022-12-04 09:55:12 -0800

    Updates to manifest support classes to support generic handling of manifests and added tests

--- jena-shex/src/main/java/org/apache/jena/shex/manifest/ValidationManifest.java
+++ jena-shex/src/main/java/org/apache/jena/shex/manifest/ValidationManifest.java
@@ -1,11 +1,22 @@
 package org.apache.jena.shex.manifest;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class ValidationManifest extends Manifest<ValidationEntry> {
     public ValidationEntry newEntry (Map<String, SourcedString> nvps) {
         ValidationEntry entry = ValidationEntry.newEntry(nvps);
         addEntry(entry);
         return entry;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
