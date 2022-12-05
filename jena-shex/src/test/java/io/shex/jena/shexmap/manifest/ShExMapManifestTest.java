e5d62b21169edfa596159b5254decf4f66a9a805
Eric Prud'hommeaux <eric+github@w3.org>
2022-12-05 23:20:41 +0100

    ValidationManifest validates two entries

--- jena-shex/src/test/java/io/shex/jena/shexmap/manifest/ShExMapManifestTest.java
+++ jena-shex/src/test/java/io/shex/jena/shexmap/manifest/ShExMapManifestTest.java
@@ -3,10 +3,6 @@ package io.shex.jena.shexmap.manifest;
 import org.apache.jena.shex.manifest.*;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
@@ -28,20 +24,6 @@ public class ShExMapManifestTest {
         assertEquals(entries.size(), 2);
         assertEquals(entries.get(0).getClass().getName(), ValidationEntry.class.getName());
         assertEquals(entries.get(1).getClass().getName(), ShExMapEntry.class.getName());
-
-        // Write it out.
-        OutputStream out = new ByteArrayOutputStream();
-        new ManifestWriter().writeJson(out, manifest);
-
-        // Implement a second manifest.
-        Manifest manifest2 = new ValidationAndShExMapManifest();
-
-        // Populate it from first's output.
-        InputStream in2 = new ByteArrayInputStream(out.toString().getBytes());
-        new ManifestReader().read(in2, base, manifest2);
-
-        // They should be equivalent.
-        assertEquals(manifest2, manifest);
     }
 
     private class ValidationAndShExMapManifest extends Manifest<ManifestEntry> {
