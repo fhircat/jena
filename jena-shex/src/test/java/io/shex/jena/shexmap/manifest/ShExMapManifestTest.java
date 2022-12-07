4eacf483a8aa43702e3eb187e856ca14b63b263a
Eric Prud'hommeaux <eric+github@w3.org>
2022-12-08 00:53:06 +0100

    + ShExMapTest

--- jena-shex/src/test/java/io/shex/jena/shexmap/manifest/ShExMapManifestTest.java
+++ jena-shex/src/test/java/io/shex/jena/shexmap/manifest/ShExMapManifestTest.java
@@ -1,8 +1,16 @@
 package io.shex.jena.shexmap.manifest;
 
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.apache.jena.graph.Graph;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.shex.*;
 import org.apache.jena.shex.manifest.*;
-import org.junit.Test;
+import org.apache.jena.sparql.graph.GraphFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
@@ -12,21 +20,51 @@ import static org.junit.Assert.assertEquals;
 public class ShExMapManifestTest {
     static String base = "./src/test/files";
 
-    @Test
-    public void RoundTripHeterogeneousShExMap() {
+    public static TestSuite suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(new ShExMapManifestTest.RoundTripHeterogeneousShExMap());
 
-        // Populate heterogeneous manifest from a file.
-        Manifest manifest = new ValidationAndShExMapManifest();
-        new ManifestReader().read(Path.of(base, "ShExMap-manifest.json"), manifest);
+        String manifestFile = "ShExMap-manifest.json";
+        ValidationAndShExMapManifest manifest = new ValidationAndShExMapManifest();
+        new ManifestReader().read(Path.of(base, manifestFile), manifest);
 
-        // Check against some structural expectations.
-        List<ManifestEntry> entries = manifest.getEntries();
-        assertEquals(entries.size(), 2);
-        assertEquals(entries.get(0).getClass().getName(), ValidationEntry.class.getName());
-        assertEquals(entries.get(1).getClass().getName(), ShExMapEntry.class.getName());
+//        OutputStream out = new ByteArrayOutputStream();
+//        new ManifestWriter().writeJson(out, manifest);
+//        System.out.println(out);
+
+        List<ManifestEntry> manifestEntries = manifest.getEntries();
+        for (int i = 0; i < manifestEntries.size(); ++i) {
+            ManifestEntry entry = manifestEntries.get(i);
+            suite.addTest(entry instanceof ValidationEntry
+                    ? new TestManifest.ValidationTest(manifestFile, i, entry)
+                    : new ShExMapTest(manifestFile, i, entry)
+            );
+        }
+
+        return suite;
     }
 
-    private class ValidationAndShExMapManifest extends Manifest<ManifestEntry> {
+//    @Test
+    static public class RoundTripHeterogeneousShExMap extends TestCase {
+
+        RoundTripHeterogeneousShExMap() {
+            super("RoundTripHeterogeneousShExMap");
+        }
+
+        public void runTest() {
+            // Populate heterogeneous manifest from a file.
+            Manifest manifest = new ValidationAndShExMapManifest();
+            new ManifestReader().read(Path.of(base, "ShExMap-manifest.json"), manifest);
+
+            // Check against some structural expectations.
+            List<ManifestEntry> entries = manifest.getEntries();
+            assertEquals(entries.size(), 2);
+            assertEquals(entries.get(0).getClass().getName(), ValidationEntry.class.getName());
+            assertEquals(entries.get(1).getClass().getName(), ShExMapEntry.class.getName());
+        }
+    }
+
+    private static class ValidationAndShExMapManifest extends Manifest<ManifestEntry> {
         @Override
         public ManifestEntry newEntry(Map<String, SourcedString> nvps) {
             ManifestEntry newEntry;
@@ -36,6 +74,35 @@ public class ShExMapManifestTest {
                 newEntry = new ValidationEntry(nvps);
             addEntry(newEntry);
             return newEntry;
+        }
+    }
+
+    static public class ShExMapTest extends TestCase {
+        private final ManifestEntry entry;
+
+        public ShExMapTest(String manifestFile, int i, ManifestEntry entry) {
+            super(getTestName(manifestFile, i, entry));
+            this.entry = entry;
+        }
+
+        static String getTestName(String manifestFile, int i, ManifestEntry entry) {
+            return manifestFile + "[" + i + "] " + entry.getDataLabel() + " \\ " + entry.getSchemaLabel();
+        }
+
+        @Override
+        public void runTest() {
+//            System.out.println(getName());
+
+            InputStream queryMap = new ByteArrayInputStream(entry.getQueryMap().getBytes());
+            ShapeMap smap = Shex.readShapeMap(queryMap, base);
+
+            InputStream dataInputStream = new ByteArrayInputStream(entry.getData().getBytes());
+            Graph graph = GraphFactory.createDefaultGraph();
+            RDFDataMgr.read(graph, dataInputStream, base, Lang.TTL);
+
+            ShexSchema schema = Shex.schemaFromString(entry.getSchema(), base);
+            ShexReport report = ShexValidator.get().validate(graph, schema, smap);
+            assertTrue(report.conforms());
         }
     }
 }
